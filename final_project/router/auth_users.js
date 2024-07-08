0f0501a474b430918e7675913905689eb328dc86
NookPawanrat <pawanrat.santiyanon@gmail>
2024-07-08 12:05:31 -0400

    Task1-8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,26 +5,113 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-//only registered users can login
+// Register a new user -- Task 6
+regd_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!isValid(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
+});
+
+
+//only registered users can login -- Task 7
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
-// Add a book review
+// Add a book review --- Task 8
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book = books[isbn]
+  if (book) {  // Check if friend exists
+    let review = req.body.review;
+    if (review) {
+        book["reviews"] = review;
+    }
+    books[isbn] = book;  
+        res.send(`Book with the ISBN ${isbn} updated.`);
+    } else {
+        // Respond if friend with specified email is not found
+        res.send("Unable to find book!"); 
+    }     
 });
+
+// Delete a book review --- Task 8
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const user = req.body.username;
+    let book = books[isbn]
+    let review_user = req.session.username
+    if (book) {  
+      if (user === review_user) {
+          delete book["reviews"];
+          res.send(`Book with the ISBN ${isbn} deleted.`);
+      } else {
+          res.send(`Cannot delete other user reveiwe.`);
+      }    
+    } else {
+        res.send("Unable to find book!"); 
+    }     
+});
+  
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
